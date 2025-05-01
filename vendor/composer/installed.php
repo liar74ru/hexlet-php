463ad4b2cd4b275d48013b772880546740dff93b
liar74ru <liar2707@gmail.com>
2025-05-01 16:57:28 +0500

    add composer log / PHP_CodeSniffer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'liar74ru/hexlet',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6f09aec5a78451514d641fc6759ccaae3badb648',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'liar74ru/hexlet' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6f09aec5a78451514d641fc6759ccaae3badb648',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.12.2',
+            'version' => '3.12.2.0',
+            'reference' => '6d4cf6032d4b718f168c90a96e36c7d0eaacb2aa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
